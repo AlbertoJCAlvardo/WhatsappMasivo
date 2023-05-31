84252b1f14bfa6f65123059d6e239fe6ac008298
Alberto Alvarado <85086650+AlbertoJCAlvardo@users.noreply.github.com>
2023-05-31 01:35:56 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+from frames.view import View
+
+def main():
+    app = View()
+    app.start_mainloop()
+
+if __name__ == "__main__":
+    main()
+
+
--- frames/root.py
+++ frames/root.py
@@ -1,13 +1,17 @@
 from ttkthemes import ThemedTk
-
+from PIL import Image,ImageTk
 class Root(ThemedTk):
     def __init__(self):
-        super().__init__(theme="adapta")
+        super().__init__(theme="equilux")
 
         start_width = 1000
         min_width = 1000
         start_height = 600
         min_height = 600
+        
+        ico = Image.open('logo.png')
+        photo = ImageTk.PhotoImage(ico)
+        self.wm_iconphoto(True,photo)
 
         self.geometry(f"{start_width}x{start_height}")
         self.minsize(width=min_width, height=min_height)
